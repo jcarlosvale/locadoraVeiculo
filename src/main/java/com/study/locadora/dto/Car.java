84b4dad7f87b621fa969ce1b96d0ba59a4ab5c51
Joao Vale <jcarlos.vale@gmail.com>
2024-03-25 13:04:04 +0000

    database e validation

--- src/main/java/com/study/locadora/dto/Car.java
+++ src/main/java/com/study/locadora/dto/Car.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.stream.Stream;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -13,26 +15,14 @@ import java.util.stream.Stream;
 @NoArgsConstructor
 public class Car {
 
+    @NotBlank(message = "placa nao pode estar vazia")
     private String placa;
+
+    @NotBlank(message = "descicao nao pode estar vazia")
+    @Size(min = 2)
     private String descricao;
+
+    @Min(2000)
     private int ano;
 
-
-    public static void main(String[] args) {
-//        var lista = List.of(1,2,3,4,5,6,7);
-
-        Stream<Double> randomNumbersStream = Stream.generate(Math::random);
-
-//        Stream<Integer> streamInteiros = Stream.of(1,2,3,4,5,6,7);
-
-        randomNumbersStream
-                .map(aDouble -> aDouble)
-                .limit(10)
-                .forEach(x -> {System.out.println(x);});
-
-
-
-
-
-    }
 }
